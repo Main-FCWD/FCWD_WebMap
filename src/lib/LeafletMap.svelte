c14d6d85005ec2ab661e47de7f79c25d949b8eeb
Tyler Stahl <stahlstradamus@gmail.com>
2023-07-17 12:56:00 -0400

    updated png src

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -418,7 +418,7 @@
 					'body > div > main > section > div > div.leaflet-control-container > div.leaflet-top.leaflet-right > div > section'
 				);
 
-				var CollapseBtn = `<div><img class="layerControlToggle" style="float:right;" src="build/Data/images"></div>`;
+				var CollapseBtn = `<div><img class="layerControlToggle" style="float:right;" src="build/Data/images/lct_button.png"></div>`;
 
 				LayerControlContainer.insertAdjacentHTML('afterbegin', CollapseBtn);
 
