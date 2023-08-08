36116396777bc174336c1b61845bea6567b29c3e
Tyler Stahl <stahlstradamus@gmail.com>
2023-08-08 11:30:11 -0400

    0.0.8: Changed warning color

--- build/_app/immutable/chunks/L.Control.Locate.0ecaaf63.js
+++ build/_app/immutable/chunks/L.Control.Locate.0ecaaf63.js
@@ -1,4 +1,4 @@
-import{g as v}from"./2.72436964.js";import{a as w}from"./leaflet-src.921230ba.js";function y(p,_){for(var s=0;s<_.length;s++){const a=_[s];if(typeof a!="string"&&!Array.isArray(a)){for(const l in a)if(l!=="default"&&!(l in p)){const r=Object.getOwnPropertyDescriptor(a,l);r&&Object.defineProperty(p,l,r.get?r:{enumerable:!0,get:()=>a[l]})}}}return Object.freeze(Object.defineProperty(p,Symbol.toStringTag,{value:"Module"}))}var f={exports:{}};/*!
+import{g as v}from"./2.d4670235.js";import{a as w}from"./leaflet-src.b2f71c75.js";function y(p,_){for(var s=0;s<_.length;s++){const a=_[s];if(typeof a!="string"&&!Array.isArray(a)){for(const l in a)if(l!=="default"&&!(l in p)){const r=Object.getOwnPropertyDescriptor(a,l);r&&Object.defineProperty(p,l,r.get?r:{enumerable:!0,get:()=>a[l]})}}}return Object.freeze(Object.defineProperty(p,Symbol.toStringTag,{value:"Module"}))}var f={exports:{}};/*!
 Copyright (c) 2016 Dominik Moritz
 
 This file is part of the leaflet locate control. It is licensed under the MIT license.
